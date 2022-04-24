45a07b635dda8ebca9f85149a04f1d50732344cd
pritanjali <pritanjalisingh13@gmail.com>
2022-04-25 00:31:10 +0100

    Update AirbnbPropertyManagementServiceApplication.java

--- src/main/java/com/pritanjalis/airbnb/AirbnbPropertyManagementServiceApplication.java
+++ src/main/java/com/pritanjalis/airbnb/AirbnbPropertyManagementServiceApplication.java
@@ -3,16 +3,23 @@ package com.pritanjalis.airbnb;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 
 @SpringBootApplication
 @EnableJpaRepositories
 @EntityScan("com.pritanjalis.airbnb.domain")
-public class AirbnbPropertyManagementServiceApplication {
+public class AirbnbPropertyManagementServiceApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AirbnbPropertyManagementServiceApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(AirbnbPropertyManagementServiceApplication.class);
 	}
 
 }
