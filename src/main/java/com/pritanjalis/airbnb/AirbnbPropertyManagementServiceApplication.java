303f604f984076efd2630f6ed6e6aae8b46527d2
pritanjali <pritanjalisingh13@gmail.com>
2022-04-25 01:29:05 +0100

    adding index.html

--- src/main/java/com/pritanjalis/airbnb/AirbnbPropertyManagementServiceApplication.java
+++ src/main/java/com/pritanjalis/airbnb/AirbnbPropertyManagementServiceApplication.java
@@ -1,11 +1,19 @@
 package com.pritanjalis.airbnb;
 
+import com.pritanjalis.airbnb.domain.AirbnbProperty;
+import com.pritanjalis.airbnb.domain.AirbnbPropertyType;
+import com.pritanjalis.airbnb.service.AirbnbPropertyManagementService;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.stream.IntStream;
 
 
 @SpringBootApplication
@@ -21,5 +29,24 @@ public class AirbnbPropertyManagementServiceApplication extends SpringBootServle
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
 		return builder.sources(AirbnbPropertyManagementServiceApplication.class);
 	}
+
+
+	//Fake index.html
+	@RequestMapping(value = "/")
+	public String indexPage() {
+		return "AirbnbPropertyManagementServiceApplication is deployed and available";
+	}
+
+
+	@Bean
+	ApplicationRunner applicationRunner(final AirbnbPropertyManagementService propertyManagementService) {
+		return args -> {
+			IntStream.range(0, 9).forEach(id -> {
+				AirbnbProperty airbnbProperty = new AirbnbProperty("IRL-WH-00"+ id, "Westmeath", 3, 2, 5, AirbnbPropertyType.FLAT, 100.00f);
+				propertyManagementService.addAirbnbProperty(airbnbProperty);
+			});
+		};
+	}
+
 
 }
